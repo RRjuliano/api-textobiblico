41137e8514dd4e1e3220ddf318d8bea0aab4bbe6
julianoRR <jubaa@live.com>
2024-10-28 14:44:12 -0300

    cors (origin: *)

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require('cors')
 const app = express()
 
 //midlleware
-app.use(cors())
+app.use(cors({ origin: '*' }))
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
