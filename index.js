19816b2a767f7b9124ad50a212c48b345452bd65
JulianoRR <30480906+JulianoRR@users.noreply.github.com>
2024-11-04 00:32:28 -0300

    Update index.js on connectDB()

--- index.js
+++ index.js
@@ -12,11 +12,13 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use('/public', express.static(__dirname + '/public'))
-
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.path} - ${req.ip}`)
     next()
 })
+
+//connection database
+connectDB()
 
 //routes
 app.use('/api/search', consultaRoute)
@@ -34,7 +36,6 @@ app.get('/', (req, res) => {res.send(
 )}) //res.sendFile(__dirname + '/views/index.html')})
 
 //server
-//connectDB()
 const port = process.env.PORT || 5000
 app.listen(port, () => {
     console.log(`Server is running : http://localhost:${port}`)
