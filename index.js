2fa83c161d39700a34b3177f9a19b08ffef531aa
JulianoRR <30480906+JulianoRR@users.noreply.github.com>
2024-11-03 23:15:24 -0300

    Update index.js comment app.listen()

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.get('/', (req, res) => {res.send(
 
 //server
 //connectDB()
-const port = process.env.PORT || 5000
-app.listen(port, () => {
-    console.log(`Server is running : http://localhost:${port}`)
-})
+//const port = process.env.PORT || 5000
+//app.listen(port, () => {
+//    console.log(`Server is running : http://localhost:${port}`)
+//})
