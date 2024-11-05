85127fe9a1a08dbc136c3a0fe87973350bd62663
JulianoRR <30480906+JulianoRR@users.noreply.github.com>
2024-11-05 03:55:07 -0300

    Update index.js conclusion

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
-const connectDB = require("./config/db.js");
 const consultaRoute = require('./routes/consulta.route.js')
 const bibliaAcfRoute = require('./routes/biblia-acf.route.js')
 const cors = require('cors')
-
 const app = express()
+require('dotenv').config()
 
 //midlleware
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-app.use('/public', express.static(__dirname + '/public'))
-app.use(cors()) //{ origin: "https://textobiblico.vercel.app", credentials: true }))
+app.use(cors({
+    origin: ['http://localhost:5173', 'https://textobiblico.vercel.app'],
+    credentials: true
+}))
+
+//app.use('/public', express.static(__dirname + '/public'))
 //app.use((req, res, next) => {
 //    console.log(`${req.method} ${req.path} - ${req.ip}`)
 //    next()
@@ -33,18 +35,19 @@ app.get('/', (req, res) => {res.send(
     <div/>`
 )}) //res.sendFile(__dirname + '/views/index.html')})
 
-//connection database
 //connectDB()
 
+//connection database
+async function main() { await mongoose.connect(process.env.MONGO_URI) }
+main().then(() => console.log("Mongodb connect successfully!")).catch(err => console.log(err));
+
 //server
-//const port = process.env.PORT || 5000
-//app.listen(port, () => {
-//    console.log(`Server is running : http://localhost:${port}`)
-//})
+const port = process.env.PORT || 5000
+app.listen(port, () => { console.log(`Server is running : http://localhost:${port}`) })
 
-const CONNECTION_URL = process.env.MONGO_URI
-const PORT = process.env.PORT|| 5000;
+//const CONNECTION_URL = process.env.
+//const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(CONNECTION_URL)
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
-  .catch((error) => console.log(`${error} did not connect`))
+//mongoose.connect(CONNECTION_URL)
+//  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+//  .catch((error) => console.log(`${error} did not connect`))
