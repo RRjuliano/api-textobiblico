1e70e279769cbbec5ea944c86006ddb6e6e1e58a
JulianoRR <jubaa@live.com>
2024-11-07 16:41:50 -0300

    update del biblia-acf/new

--- controllers/biblia-acf.controller.js
+++ controllers/biblia-acf.controller.js
@@ -1,25 +1,5 @@
 const BibliaAcf = require('../models/biblia-acf.model.js')
 
-const newBibliaAcf = async (req, res) => {
-
-    const cod = req.body.cod
-    const value = req.body.value
-    console.log(req.body)
-   
-    if(!cod || typeof cod != "string" || !value || typeof value != "string"){
-        console.log({response: false})
-        return res.status(404).json({ success: false, message:'erro no formato da request', error: '1'})
-    }
-    try {
-        const item =  new BibliaAcf({cod, value})
-        const result = await item.save()
-        console.log({response: true})
-        res.status(200).json({ success: true, value: item, result: result})
-    } catch (error){
-        console.log({response: false})
-        res.status(500).json({ success: false, message:'server error', error: '2'})
-    }
-}
 const getBibliaAcf = async(req, res) => {
 
     const {cod} = req.params
@@ -32,5 +12,4 @@ const getBibliaAcf = async(req, res) => {
     }
 }
 
-
-module.exports = {newBibliaAcf, getBibliaAcf}
+module.exports = { getBibliaAcf }
--- routes/biblia-acf.route.js
+++ routes/biblia-acf.route.js
@@ -1,9 +1,8 @@
 const express = require('express')
 
 const router = express.Router()
-const {newBibliaAcf, getBibliaAcf} = require('../controllers/biblia-acf.controller.js')
+const {getBibliaAcf} = require('../controllers/biblia-acf.controller.js')
 
-router.post('/new', newBibliaAcf)
 router.get('/:cod', getBibliaAcf)
 router.get('/', (req, res) => {res.send(
     '<br><br><br><h2 style="width: 100%; height: 100%; text-align:center;">Bem-vindo a biblia-acf route !<h2/>'
