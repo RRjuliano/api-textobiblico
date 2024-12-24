52d36262b2a3c23b7665150c4502bd5196bc3ba5
JulianoRR <jubaa@live.com>
2024-12-24 14:08:30 -0300

    reg main final

--- controllers/consulta.controller.js
+++ controllers/consulta.controller.js
@@ -1,7 +1,6 @@
 const Consulta = require('../models/consulta.model.js')
 const mongoose= require('mongoose')
 
-//const {ERlivro, ERnum, ERcpvs, base, lvs_orig, lvs, lvs_abre, lvs_ace, n_caps, n_vers} = require('../data/data.js')
 const {main} = require('../machine/machine.js')
 
 
@@ -14,7 +13,7 @@ const newConsulta = async (req, res) => {
         return res.status(404).json({ success: false, message:'erro no formato da request'})
     }
     try {
-        const response = await main(input) //findRefAll
+        const response = await main(input)
         const status = response?true:false
         if(!status){
             res.status(200).json({ success: false, message: 'nenhum texto encontrado'})
@@ -52,192 +51,5 @@ const updateConsulta = async (req, res) => {
 const deleteConsulta = async(req, res) => {
 
 }
-
-function findRef(input) {
-    let i1 = input.replace("II ", " 2 ")
-    i1 = i1.replace("I ", " 1 ")
-    i1 = i1.toLowerCase().replace('á', 'a').replace('é', 'e').replace('í', 'i').replace('ó', 'o').replace('ú', 'u').replace('â', 'a').replace('ã', 'a').replace('ê', 'e').replace('ô', 'o').replace('õ', 'o').replace('ç', 'c')
-    let item = []
-    let iEnd = 0
-    ///////////////////////////proc1 - livro
-    let proc1 = i1.match(ERlivro)
-    if (!proc1) {
-        iEnd = input.length
-        return ["Livro não encontrado !!!", iEnd]
-    }
-    let livro = proc1[0]
-    iEnd = proc1.index + livro.length
-
-    let ind_livro = lvs.indexOf(livro)
-    let tipo = "nome"
-    if (ind_livro == -1) {
-        ind_livro = lvs_abre.indexOf(livro)
-        tipo = "abrev"
-    }
-    if (livro == 'jo' && input.toLowerCase().substring(proc1.index, proc1.index+2) == 'jó') {
-        ind_livro = lvs.indexOf('jó')
-        livro = 'jó'
-        tipo = "nome"
-    }
-    if(livro == "salmo"){
-        ind_livro = lvs.indexOf('salmos')
-        livro = 'salmos'
-        tipo = "nome"
-        iEnd += 1
-    }
-    if(livro == "proverbio"){
-        ind_livro = lvs.indexOf('proverbios')
-        livro = 'proverbios'
-        tipo = "nome"
-        iEnd += 1
-    }
-    if (ind_livro == -1) {
-        return ["Livro inválido !!!", iEnd]
-    }
-    ////////////////////////////proc2 - numero do livro
-    let i2 = i1.substring(0, proc1.index)
-    let proc2 = i2.match(ERnum)
-    if (proc2) {
-        let num = proc2[0]
-        let q = lvs
-        if (tipo == "abrev") {
-            q = lvs_abre
-        }
-        if (num <= 3 && num > 0) {
-            if (num > 1) {
-                ind_livro = q.indexOf(livro, ind_livro + 1)
-            }
-            if (num > 2) {
-                ind_livro = q.indexOf(livro, ind_livro + 1)
-            }
-            if (livro == 'joao' || livro == 'jo') {
-                ind_livro = q.indexOf(livro, ind_livro + 1)
-            }
-        } else {
-            return ["Número do Livro inválido !!!", iEnd]
-        }
-        item.push(ind_livro)
-        item.push(parseInt(num))
-    } else {
-        item.push(ind_livro)
-        item.push(null)
-    }
-    if (ind_livro == -1) {
-        return ["Número do Livro inválido !!!", iEnd]
-    }
-    ///////////////////////////proc3 - capitulo e versiculos 
-    let i3 = i1.substr(proc1.index + livro.length)
-    procLetra = i3.match(ERlivro)
-    if (procLetra) {
-        i3 = i3.substr(0, procLetra.index)
-    }
-    do {
-        proc3 = i3.match(ERcpvs)
-        if (proc3) {
-            let num = proc3[0]
-            iEnd += proc3.index + num.length
-            item.push(parseInt(num)-1)
-            i3 = i3.substr(proc3.index + num.length)
-        }
-    } while (proc3 && item.length < 5)
-      if (item.length > 2) {
-        if (n_caps[ind_livro] <= item[2]) {
-            return ["Capitulo inválido !!!", iEnd]
-        }
-        if (item.length > 3) {
-            if (n_vers[ind_livro][item[2]] < item[3]) {
-                return ["Número do versiculo inválido !!!", iEnd]
-            }
-            if (item.length == 5) {
-                if (item[4] <= item[3] || n_vers[ind_livro][item[2]] < item[4]) {
-                    return ["Intervalo de versiculos inválido !!!", iEnd]
-                }
-            }
-        }
-    } else {
-        return ["Apenas o livro, qual capitulo??", iEnd]
-    }
-    return [{"lv": item[0], "nLv": item[1], "cap": item[2], "vers": item[3], "versI": item[4]}, iEnd]
-}
-async function process(i) {
-    const url = base + lvs_ace[i.lv] + '_' + (i.cap+1)
-    let ref = lvs_orig[i.lv] + ' ' + (i.cap+1)
-    let cod = i.lv + '_' + i.cap
-    let cap = true
-    let tex = ''
-    if(i.vers === undefined){ //cap
-        const end = n_vers[i.lv][i.cap]
-        let c = 0
-        var cods = []
-        do {
-            cods.push(`${i.lv}_${i.cap}_${c}`)
-            c++
-        } while (c < end)
-    } else { 
-        if(i.versI === undefined){ //vers
-            cap = false
-            ref += " : " + (i.vers+1)
-            var cods = [`${i.lv}_${i.cap}_${i.vers}`]
-            cod += "_" + (i.vers)
-        } else { //int. vers
-            cap = false
-            ref += " : " + (i.vers+1) + " - " + (i.versI+1)
-            cod += "_" + i.vers + "_" + i.versI
-            let vs = i.vers
-            var cods = []
-            do {
-                cods.push(`${i.lv}_${i.cap}_${vs}`)
-                vs++
-            } while (vs <= i.versI)
-        }
-    }
-    
-    ref += ' ARC'
-
-    async function getBibliaAcf(cod) {
-        let response = await fetch(`https://api-textobiblico.vercel.app/api/biblia-acf/${cod}`)
-        let data = await response.json()
-        return data                              
-	}
-
-    async function getText(cods) {
-
-        async function executeQueue() {
-            let arr = []
-            for (let promiseFunc of promises) {
-                const result = await promiseFunc()
-                if(result.success) { arr.push(result.value.value) }
-            }
-            return arr.join(' ')
-        }
-
-        let promises = []
-        cods
-            .map((cod)=> {
-                promises.push(() => new Promise(resolve => setTimeout(() => resolve(getBibliaAcf(`${cod}`)), 5)))
-            })
-        const text = await executeQueue()
-        return text
-    }
-
-    tex = await getText(cods)
-    tex = (( i.versI === undefined) && (i.vers !== undefined)) ? tex.substring(tex.match(/ /).index) : tex
-
-    return {"text": tex, "ref": ref, "url": url, "cod": cod}
-}
-async function findRefAll(input) {
-    let inp = input
-    let response = []
-    do {
-        let[i,iEnd] = findRef(inp)
-        if (typeof(i) != 'string') {
-            console.log(i)
-            response.push(await process(i))
-        }
-        inp = inp.substr(iEnd)
-    } while (inp.length > 0)
-    return response.length==0?false:response
-}
-
 
 module.exports = {newConsulta, getConsulta}
--- machine/machine.js
+++ machine/machine.js
@@ -1,23 +1,38 @@
 const { base, lvs_orig, lvs, lvs_abre, lvs_ace } = require('../data/data.js')
 
-async function execAll(str){
 
-    const string = str.toLowerCase().replaceAll("I ", " 1 ").replace("II", " 2 ").replaceAll('á', 'a').replaceAll('é', 'e').replaceAll('í', 'i').replaceAll('ó', 'o').replaceAll('ú', 'u').replaceAll('â', 'a').replaceAll('ã', 'a').replaceAll('ê', 'e').replaceAll('ô', 'o').replaceAll('õ', 'o').replaceAll('ç', 'c')
-    const reg = /(?<num>\d?)\s?(?<liv>[A-Z]+)[\s\W]*(?<cap>\d+)[\s\W]*(?<vers>\d+)?[\s\W]*(?<vers_>\d+)?/i
+async function excep_treat(str){ //treatrement initial
+
+    //exceptions - livro jó, salmo e proverbio, cantares de salomao e lamentaçoes de jeremias
+    //treatament - tolowercase, numeros romanos, acentos em vogais, cedilha
+
+    const str_treated = str.replaceAll(/\bII/g, "   2").replaceAll(/\bI/g, "   1")
+        .toLowerCase().replaceAll('jó', 'joh')
+        .replaceAll(' 1saias', 'isaias')
+        .replaceAll('á', 'a').replaceAll('é', 'e').replaceAll('í', 'i').replaceAll('ó', 'o').replaceAll('ú', 'u')
+        .replaceAll('â', 'a').replaceAll('ã', 'a').replaceAll('ê', 'e').replaceAll('ô', 'o').replaceAll('õ', 'o').replaceAll('ç', 'c')
+        .replaceAll("salmo ", 'salmos ').replaceAll("proverbio ", 'proverbios ')
+        .replaceAll('cantares de salomao', 'cantares').replaceAll('lamentacoes de jeremias', 'lamentacoes')
+
+    return str_treated
+}
+async function execAll(str){ //find all reference
+
+    const reg = /(?<num>\d?)\s?(?<liv>[A-Z]{2,})[\D\W]{0,3}(?<cap>\d{1,3})([\D\W]{0,3}(?<vers>\d{1,3}))?([\D\W]{1,3}(?<vers_>\d{1,3}))?/i
     let index = 0
     let list = []
     do {
 
-        let res = reg.exec(string.substring(index))
+        let res = reg.exec(str.substring(index))
         if(!res){ break }
 
         index += res.index + res[0].length
 
         const g = res.groups
-        if (g.vers && g.vers_ && (g.vers_ <= g.vers)){ index -= g.vers_.length ; g.vers_ = null }
+        if ( g.vers_ && (g.vers_ <= g.vers)){ index -= g.vers_.length ; g.vers_ = null }
         list.push(g)
 
-    } while ( index !== string.length )
+    } while ( index !== str.length )
     
     console.log(list)
     return list.length==0?false:list
@@ -28,56 +43,55 @@ async function getText(cod){ //called getBibliaAcf(cod)
     const tex = result.success? result.value.value : ''
     return tex
 }
-async function getBibliaAcf(cod) {
+async function getBibliaAcf(cod) { //request to api
     let response = await fetch(`https://api-textobiblico.vercel.app/api/biblia-acf/${cod}`)
     let data = await response.json()
     return data                              
 }
-async function process(i) {
+async function process(i) { //called getText
 
-    const liv = i.liv
-    let nLiv = lvs.indexOf(liv)
-    let tipo = "nome"
-    
-    if (nLiv == -1) {
-        nLiv = lvs_abre.indexOf(liv)
-        tipo = "abrev"
-    }
-
-    if(nLiv == -1) { return false }
-    console.log(i.vers)
+    //não aceita capitulo ou versiculo igual a 0, retorna false
     if(Number(i.cap) == 0 || (i.vers && Number(i.vers) == 0)) { return false }
 
-    const url = base + lvs_ace[nLiv] + '_' + i.cap
-    let ref = lvs_orig[nLiv] + ' ' + i.cap
+    //indice do livro, verifica por nome ou abreviatura
+    let nLiv = lvs.indexOf(i.liv)
+    if (nLiv == -1) { nLiv = lvs_abre.indexOf(i.liv) }
+
+    //verifica se há numero do livro (pular indice), observar livro joao e cartas (mesmo nome - pular indice)
+    if(nLiv != -1 && i.num){ 
+        if(i.num == '2') { nLiv = lvs.indexOf(lvs[nLiv], nLiv+1) }
+        if(nLiv != -1 && i.num == '3') { nLiv = lvs.indexOf(lvs[nLiv], nLiv+1) }
+        if(nLiv != -1 && lvs[nLiv] == 'joao') { nLiv = lvs.indexOf(lvs[nLiv], nLiv+1) }
+    }
+
+    //se não encontrar indice retorna false
+    if(nLiv == -1) { return false }
+
+    const url = base + lvs_ace[nLiv] +'_' + i.cap
+    let ref = '( ' +lvs_orig[nLiv] +' ' + i.cap
     let cod = nLiv + '_' + (Number(i.cap)-1)
-    let cap = true
     let result = ''
     let tex = ''
-    let cods = []
 
     if(i.vers === undefined){ //cap
-        console.log(cod)
         result = await getBibliaAcf(cod)
         tex = result.success? result.value.value : ''
     } else { 
         if(i.vers_ == undefined){ //vers
             cap = false
-            ref += " : " + i.vers
+            ref += ":" + i.vers
             cod += "_" + (Number(i.vers)-1)
-            console.log(cod)
             result = await getBibliaAcf(cod)
             tex = result.success? result.value.value : ''
         } else { //interval vers
             cap = false
-            ref += " : " + i.vers + " - " + i.vers_
-            cod += "-" + (Number(i.vers)-1) + "-" + (Number(i.vers_)-1)
-            console.log(cod)
+            ref += ":" + i.vers + "-" + i.vers_
+            cod += "-" + (Number(i.vers)-1) + "-" + (Number(i.vers_)-1)+")"
             result = await getBibliaAcf(cod)
             tex = result.success? result.value.value : ''
         }
     }
-    ref += ' ARC'
+    ref += ' ARC )'
 
     if ( tex == '') { return false }
     tex = tex.substring(1)
@@ -86,15 +100,15 @@ async function process(i) {
     return {"text": tex, "ref": ref, "url": url, "cod": cod}
 }
 
-async function main(str) {
+async function main(str) { //called excep_treat, execAll, process
     
-    //function exec of object regex to find referencias
-    const list = await execAll(str)
+    //run execAll - loop regex exec to find occurrences
+    const list = await execAll( await excep_treat(str)) //before run excetions
     if(!list){ return false }
 
     let proc = []
     for (e of list){ proc.push( await process(e) ) }
-    proc = proc.filter((x) => x !== false)
+    proc = proc.filter( (x) => x !== false )
     console.log(proc)
     return proc.length==0?false:proc
 }
